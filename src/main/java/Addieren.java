8664f05ffd0878949bd0c5bc3c7fe770fe6130da
nobody1337 <98647065+nobody1337@users.noreply.github.com>
2022-06-15 13:58:09 +0200

    Initial commit

---
+++ src/main/java/Addieren.java
@@ -0,0 +1,10 @@
+public class Addieren {
+
+    public static void main(String[] args) {
+        System.out.println(plus(5,5));
+    }
+
+    public static int plus(int x, int y){
+        return x+y;
+    }
+}
---
+++ src/test/java/Unittest.java
@@ -0,0 +1,9 @@
+import org.junit.*;
+import static org.junit.Assert.*;
+public class Unittest {
+    @Test
+    public void test(){
+        assertEquals(10,Addieren.plus(5,5));
+    }
+
+}
